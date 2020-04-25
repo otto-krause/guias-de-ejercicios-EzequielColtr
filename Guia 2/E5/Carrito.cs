b26e3a445524ac0759d3d05b2f96d22f8b598982
Ezequiel <ezecolter@gmail.com>
2020-04-25 19:34:42 -0300

    No me salio

---
+++ Guia 2/E5/Carrito.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using System;
+
+namespace E5
+{
+    public class Carrito
+    {
+        List<Producto> carrito = new List<Producto>();
+        
+    }
+}
---
+++ Guia 2/E5/SuperMercado.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using System;
+
+namespace E5
+{
+    public class SuperMercado
+    {
+        List<Carrito> Dia = new List<Carrito>();
+
+        
+        public int Ganancias()
+        {
+            foreach (var i in Dia)
+            {
+                foreach (var j in carrito)
+                {
+                
+                }
+            }
+        }
+    
+    }
+
+}
