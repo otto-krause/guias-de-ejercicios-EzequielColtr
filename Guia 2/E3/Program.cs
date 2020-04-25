fb3d6b0d5689545eacaf22ccb9c93ce382e238c8
Ezequiel <ezecolter@gmail.com>
2020-04-25 18:24:00 -0300

    Termina3 Ej3

--- Guia 2/E3/Program.cs
+++ Guia 2/E3/Program.cs
@@ -6,7 +6,35 @@ namespace E3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            string nombre;
+            string posicion;
+            string pais;
+            int idNum;
+
+
+            Album FutbolArgentino = new Album();
+            
+
+            Console.WriteLine("Ingrese un nombre");
+            nombre = Console.ReadLine();
+
+            Console.WriteLine("Ingrese una posicion(Delantero, Mediocampista, Defensa)");
+            posicion = Console.ReadLine();
+
+            Console.WriteLine("Ingrese un pais");
+            pais = Console.ReadLine();
+
+            Console.WriteLine("Ingrese el numero de la figurita");
+            idNum = Int32.Parse(Console.ReadLine());
+
+
+
+            FutbolArgentino.IngresarFigurita(nombre,posicion,pais,idNum);
+
+            FutbolArgentino.CuantasPosicionesHay();
+
+            FutbolArgentino.EstaCompleto();
         }
     }
 }
