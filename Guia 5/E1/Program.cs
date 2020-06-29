ef1414adca6aa4cb511cb864f30cef58e210f644
EzequielColtr <63217026+EzequielColtr@users.noreply.github.com>
2020-06-29 16:12:15 -0300

    temrinado

--- Guia 5/E1/Program.cs
+++ Guia 5/E1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace E1
 {
@@ -6,7 +8,34 @@ namespace E1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int cont = 0;
+            
+            List<Persona> personas = new List<Persona>();
+
+            personas.Add(new Persona(18,"Pepe","Ortega"));
+            personas.Add(new Persona(20,"Juan","Ortega"));
+            personas.Add(new Persona(8,"Nahuel","Perez"));
+
+            Console.WriteLine("Mayores de edad:");
+            personas.Where(persona => persona.Edad >= 18)
+            .ToList()
+            .ForEach(persona => Console.WriteLine(persona.Nombre));
+
+            Console.WriteLine("Personas que se llaman Juan:");
+            personas.Where(persona => persona.Nombre == "Juan")
+            .ToList()
+            .ForEach(persona => Console.WriteLine(persona.Nombre + " " + persona.Apellido));
+
+            Console.WriteLine("Cantidad de personas que se apellidan Perez:");
+            foreach (var i in personas)
+            {
+                if(i.Apellido == "Perez")
+                {
+                    cont++;
+                }
+            }
+            Console.WriteLine(cont);
+            
         }
     }
 }
