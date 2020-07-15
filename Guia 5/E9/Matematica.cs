7785d6822bc7218936c27cdcd0bc69968dc4ee73
EzequielColtr <63217026+EzequielColtr@users.noreply.github.com>
2020-07-15 14:49:55 -0300

    terminado

---
+++ Guia 5/E9/Matematica.cs
@@ -0,0 +1,79 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace E9
+{
+    public static class Matematica
+    {
+
+        public static List<int> Primos(List<int> numeros)
+        {
+            List<int> NumerosPrimos = new List<int>();
+
+            NumerosPrimos = numeros.Where(x => x % x == 0 && x % 1 == 0)
+            .ToList();
+
+            return NumerosPrimos;
+        }
+
+        public static List<int> Pares(List<int> numeros)
+        {
+            List<int> NumerosPares = new List<int>();
+
+            NumerosPares = numeros.Where(x => x % 2 == 0)
+            .ToList();
+
+            return NumerosPares;
+        }
+
+        public static List<int> Aplanar(List<int> numeros)
+        {
+            List<int> NumerosNoRepetidos = new List<int>();
+
+            NumerosNoRepetidos = numeros.Distinct().ToList();
+
+            return NumerosNoRepetidos;
+        }
+
+        public static int Suma(List<int> numeros)
+        {
+            int Suma = 0;
+
+            numeros.ForEach(x => Suma += x);
+
+            return Suma;
+        }
+
+        public static int SumaPrimos(List<int> numeros)
+        {
+            int Suma = 0;
+            List<int> NumerosPrimos = new List<int>();
+
+            NumerosPrimos = Matematica.Primos(numeros);
+
+            NumerosPrimos.ForEach(x => Suma += x);
+
+            return Suma;
+        }
+
+        public static int Mayor(List<int> numeros)
+        {
+            List<int> NumerosOrdenadosAscendente = new List<int>();
+
+            NumerosOrdenadosAscendente = numeros.OrderByDescending(x => x).ToList();
+
+            return NumerosOrdenadosAscendente[0];
+        }
+
+        public static int Menor(List<int> numeros)
+        {
+            List<int> NumerosOrdenadosDescendiente = new List<int>();
+
+            NumerosOrdenadosDescendiente = numeros.OrderByDescending(x => x).ToList();
+
+            return NumerosOrdenadosDescendiente[(NumerosOrdenadosDescendiente.Count)-1];
+        }
+
+
+    }
+}
---
+++ Guia 5/E9/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace E9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numeros = new List<int>();
+
+            int num = 1;
+
+            Console.WriteLine("Ingrese un numero hasta ingresar un 0");
+
+            while (num != 0)
+            {
+                num = Int32.Parse(Console.ReadLine());
+
+                
+
+                if(num != 0)
+                {
+                    numeros.Add(num);
+                }
+            }
+
+            Console.WriteLine("Todos los numeros");
+            numeros.ForEach(x => Console.WriteLine(x));
+
+            Console.WriteLine("\nCuantos numeros pares hay");
+            Console.WriteLine(Matematica.Pares(numeros).Count);
+
+            Console.WriteLine("\nEl primer numero par");
+            Console.WriteLine(Matematica.Pares(numeros).Min()); 
+
+
+            Console.WriteLine("\nTodos los numeros mayores a 50");
+            numeros.Where(x => x > 50)
+            .ToList()
+            .ForEach(x => Console.WriteLine(x));
+
+            Console.WriteLine("\nCuantos numeros mayores a 50 hay");
+            Console.WriteLine(numeros.Where(x => x > 50).ToList().Count);           
+
+        }
+    }
+}
