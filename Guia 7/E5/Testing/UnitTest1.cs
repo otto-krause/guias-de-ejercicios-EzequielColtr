a4d4ac656b1f73be226b1469be94dcddbac50afb
EzequielColtr <63217026+EzequielColtr@users.noreply.github.com>
2020-11-05 18:15:40 -0300

    bien

--- Guia 7/E5/Testing/UnitTest1.cs
+++ Guia 7/E5/Testing/UnitTest1.cs
@@ -1,18 +1,31 @@
 using NUnit.Framework;
+using Ejercicio;
+using System;
 
 namespace Testing
 {
     public class Tests
     {
+
+        private Heroe Endeavor;
+
+        private Quirk HellFlame;
+
         [SetUp]
         public void Setup()
         {
+            HellFlame = new Quirk(50,false,"HellFlame");
+
+            Endeavor = new Heroe(HellFlame,15,0,true);
+
         }
 
         [Test]
-        public void Test1()
+        public void QuirkEsPeligroso()
         {
-            Assert.Pass();
+            Assert.IsFalse(HellFlame.esPeligroso());
         }
+
+        
     }
 }
