43e7e15ede34930ffc508951788f1fc376af6ccd
EzequielColtr <63217026+EzequielColtr@users.noreply.github.com>
2020-11-09 20:40:27 -0300

    prueba

---
+++ Guia 8/E1/Ejercicio/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace Ejercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pelicula;
+            
+            Console.WriteLine("Escribir una pelicula");
+            pelicula = Console.ReadLine();
+
+            ComandosPeliculas.ingresarPeliculas(pelicula);
+            //ComandosPeliculas.verTodasLasPeliculas();
+            ComandosPeliculas.verTodasLasPeliculasSinRepetidos();
+
+        }
+    }
+}
